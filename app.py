4e50a25ef7929f5f37d65fc905eb842a71e48c17
jonassteinberg1 <jsteinberg@breadfinance.com>
2022-02-22 18:48:26 -0600

    pod working; db down logging close to working

--- app.py
+++ app.py
@@ -5,37 +5,44 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import time
 
 # import the connect library from psycopg2
 from psycopg2 import connect
 
-current_datetime = datetime.datetime.now()
-current_time = current_datetime.strftime('%T CT')
-
 table_name = "test_table"
 
 # declare connection instance
-conn = connect(
-    dbname = "test_db",
-    user = "jonas",
-    host = "minor-version-upgrade-test-aurora-cluster-primary.cluster-c4lg5bob8cbp.us-west-2.rds.amazonaws.com",
-    password = "password123456789"
-)
+while True:
+    try:
+        conn = connect(
+            dbname = "test_db",
+            user = "jonas",
+            host = "minor-version-upgrade-test-aurora-cluster-primary.cluster-c4lg5bob8cbp.us-west-2.rds.amazonaws.com",
+            password = "password123456789"
+        )
 
-# declare a cursor object from the connection
-cursor = conn.cursor()
+        if conn:
+            # declare a cursor object from the connection
+            cursor = conn.cursor()
 
-# execute an SQL statement using the psycopg2 cursor object
-cursor.execute(f"INSERT into {table_name} (time) VALUES ('{current_time}')")
-conn.commit()
-cursor.execute(f"SELECT * from {table_name}")
+            # format time as 'HH:MM:SS CT'
+            current_datetime = datetime.datetime.now()
+            current_time = current_datetime.strftime('%T CT')
 
-# enumerate() over the PostgreSQL records
-for i, record in enumerate(cursor):
-    print(record[1])
+            # execute an SQL statement using the psycopg2 cursor object
+            cursor.execute(f"INSERT into {table_name} (time) VALUES ('{current_time}')")
+            # not the same time, but oh well
+            print(f"[INFO]: database up at {current_time}")
+            
+            # actually have to commit changes, doh!
+            conn.commit()
+            time.sleep(1)
 
-# close the cursor object to avoid memory leaks
-cursor.close()
+            # close the cursor object to avoid memory leaks
+            cursor.close()
 
-# close the connection as well
-conn.close()
+            # close the connection as well
+            conn.close()
+    except psycopg2.Error as e:
+        print(f"[ERROR]: database down at {current_time}")
