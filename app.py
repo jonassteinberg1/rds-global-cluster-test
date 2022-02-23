b3d3ae04f5e5c4dd85f26885565f3469b578ce03
jonassteinberg1 <jsteinberg@breadfinance.com>
2022-02-22 19:01:50 -0600

    db down logging working

--- app.py
+++ app.py
@@ -5,44 +5,46 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import psycopg2
 import time
 
-# import the connect library from psycopg2
-from psycopg2 import connect
 
 table_name = "test_table"
 
 # declare connection instance
 while True:
     try:
-        conn = connect(
+        conn = psycopg2.connect(
             dbname = "test_db",
             user = "jonas",
-            host = "minor-version-upgrade-test-aurora-cluster-primary.cluster-c4lg5bob8cbp.us-west-2.rds.amazonaws.com",
+            host = "inor-version-upgrade-test-aurora-cluster-primary.cluster-c4lg5bob8cbp.us-west-2.rds.amazonaws.com",
             password = "password123456789"
         )
-
-        if conn:
-            # declare a cursor object from the connection
-            cursor = conn.cursor()
-
-            # format time as 'HH:MM:SS CT'
-            current_datetime = datetime.datetime.now()
-            current_time = current_datetime.strftime('%T CT')
-
-            # execute an SQL statement using the psycopg2 cursor object
-            cursor.execute(f"INSERT into {table_name} (time) VALUES ('{current_time}')")
-            # not the same time, but oh well
-            print(f"[INFO]: database up at {current_time}")
-            
-            # actually have to commit changes, doh!
-            conn.commit()
-            time.sleep(1)
-
-            # close the cursor object to avoid memory leaks
-            cursor.close()
-
-            # close the connection as well
-            conn.close()
-    except psycopg2.Error as e:
+    except psycopg2.OperationalError as e:
+        # format time as 'HH:MM:SS CT'
+        current_datetime = datetime.datetime.now()
+        current_time = current_datetime.strftime('%T CT')
         print(f"[ERROR]: database down at {current_time}")
+        time.sleep(1)
+    else:
+        # declare a cursor object from the connection
+        cursor = conn.cursor()
+
+        # format time as 'HH:MM:SS CT'
+        current_datetime = datetime.datetime.now()
+        current_time = current_datetime.strftime('%T CT')
+
+        # execute an SQL statement using the psycopg2 cursor object
+        cursor.execute(f"INSERT into {table_name} (time) VALUES ('{current_time}')")
+        # not the same time, but oh well
+        print(f"[INFO]: database up at {current_time}")
+        
+        # actually have to commit changes, doh!
+        conn.commit()
+        time.sleep(1)
+
+        # close the cursor object to avoid memory leaks
+        cursor.close()
+
+        # close the connection as well
+        conn.close()
